6b9965ce26d6e0e7d10a8b34db445eda157f18c7
Raj Ekambaram <raje@EkambaramRajMBP.local>
2020-12-02 13:09:38 -0600

    Update

--- config/server.js
+++ config/server.js
@@ -1,9 +1,9 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 8080),
+  port: env.int('PORT', 8080 ),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', '4e5172a4f08275cdc8800187e6432385'),
+      secret: env('ADMIN_JWT_SECRET', '4e5172a4f08275cdc8800187e6432385'), 
     },
   },
 });
