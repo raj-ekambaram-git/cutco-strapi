02ef09d8940fe8d149793bf7c79600416094ce83
Raj Ekambaram <raje@EkambaramRajMBP.local>
2020-12-02 11:46:02 -0600

    Updated the package.jspn

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  port: env.int('PORT', 8080),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '4e5172a4f08275cdc8800187e6432385'),
