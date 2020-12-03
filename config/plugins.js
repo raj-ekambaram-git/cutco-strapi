0ef5bb20a5a0c527299bea7eeaaa9f91793e1832
Raj Ekambaram <raje@EkambaramRajMBP.local>
2020-12-03 14:09:12 -0600

    Updated the plugin values

--- config/plugins.js
+++ config/plugins.js
@@ -2,11 +2,11 @@ module.exports = ({ env }) => ({
   upload: {
     provider: 'aws-s3',
     providerOptions: {
-      accessKeyId: env(''),
-      secretAccessKey: env(''),
-      region: env('us-east-2'),
+      accessKeyId: env('AWS_ACCESS_KEY_ID', 'AWS_ACCESS_KEY_ID'),
+      secretAccessKey: env('AWS_SECRET_ACCESS_KEY', 'AWS_SECRET_ACCESS_KEY'),
+      region: env('AWS_REGION', 'AWS_REGION'),
       params: {
-        Bucket: env('cutco-strapi-images'),
+        Bucket: env('S3_MLL_BUCKET_NAME', 'S3_MLL_BUCKET_NAME'),
       },
     },
   },
