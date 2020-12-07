5e01da077b8aa2e893514d9af993592fb2411981
Raj Ekambaram <raje@EkambaramRajMBP.local>
2020-12-07 09:59:28 -0600

    Updated the db name

--- config/database.js
+++ config/database.js
@@ -7,7 +7,7 @@ module.exports = ({ env }) => ({
         client: 'postgres',
         host: env('DATABASE_HOST', 'cutco-strapi-qa.cyppfapaayzp.us-east-2.rds.amazonaws.com'),
         port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'cutcostrapi'),
+        database: env('DATABASE_NAME', 'cutcosttrapi_dev'),
         username: env('DATABASE_USERNAME', 'postgres'), 
         password: env('DATABASE_PASSWORD', 'CutcoStrapi123'),
       },
