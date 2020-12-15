63f8273e516fd06621e850b2786e0a56c7018490
raj-ekambaram-git <75325809+raj-ekambaram-git@users.noreply.github.com>
2020-12-15 17:52:46 -0600

    Update database.js
    Commited

--- config/env/dev/database.js
+++ config/env/dev/database.js
@@ -7,7 +7,7 @@ module.exports = ({ env }) => ({
         client: 'postgres',
         host: env('DATABASE_HOST', 'curco-strapi-dev.ci4mhwxnnrhf.us-east-1.rds.amazonaws.com'),
         port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'testdb1'),
+        database: env('DATABASE_NAME', 'cutcostrapi_dev'),
         username: env('DATABASE_USERNAME', 'postgres'), 
         password: env('DATABASE_PASSWORD', 'CutcoStrapi123'),
       },
